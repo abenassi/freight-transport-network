dd2e407196d228199e9b35e136abdf04b7a068fe
Agustín Benassi <agusbenassi@gmail.com>
2014-11-25 12:44:06 -0300

    Move Path classes into a new "path" module.

--- modules/builder/components/__init__.py
+++ modules/builder/components/__init__.py
@@ -1,4 +1,5 @@
-from od import OD, Path
+from od import OD
+from path import Path
 from link import RailwayLink, RoadwayLink
 from parameter import Parameter
 from railway_rolling_material import RollingMaterial
